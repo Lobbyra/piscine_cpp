479e7b7c36eab17df1b32226aa74ec6951cf1759
lobbyra <jecaudal@student.42.fr>
2020-11-09 09:01:50 +0100

    :arrow_up: feat : cpp1 ex07 bug fix and add a test file

--- cpp1/ex07/replace.cpp
+++ cpp1/ex07/replace.cpp
@@ -6,39 +6,65 @@
 /*   By: jecaudal <jecaudal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 11:13:44 by jecaudal          #+#    #+#             */
-/*   Updated: 2020/10/07 12:51:55 by jecaudal         ###   ########.fr       */
+/*   Updated: 2020/11/09 08:58:43 by jecaudal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-int		replace(std::string path, std::string s1, std::string s2)
+static int	parsing(std::string s1, std::string s2)
 {
-	size_t			index;
+	if (s1.compare("") == 0 || s2.compare("") == 0)
+	{
+		std::cerr << "ERROR : argumemts must not be empty !" << std::endl;
+		return (1);
+	}
+	return (0);
+}
+
+static std::ofstream	dot_replace_open(std::string path)
+{
 	std::ofstream	o_file;
+
+	o_file.open(path);
+	if (!(o_file.is_open()))
+		std::cerr << "ERROR : Failed to create .replace file" << path << std::endl;
+	return (o_file);
+}
+
+static std::ifstream	src_open(std::string path)
+{
 	std::ifstream	i_file;
-	std::string		buf = "init";
 
 	i_file.open(path);
 	if (!(i_file.is_open()))
-	{
 		std::cerr << "ERROR OPEN : " << path << std::endl;
+	return (i_file);
+}
+
+int		replace(std::string path, std::string s1, std::string s2)
+{
+	std::string		buf;
+	size_t			index;
+	std::ofstream	o_file;
+	std::ifstream	i_file;
+	bool			replacement_aborted;
+
+	if (parsing(s1, s2) != 0)
 		return (1);
-	}
-	o_file.open(path + ".replace");
-	if (!(o_file.is_open()))
-	{
-		std::cerr << "ERROR : Failed to create .replace file" << path << std::endl;
+	replacement_aborted = (s1 == s2);
+	i_file = src_open(path);
+	if (i_file.is_open() == false)
 		return (1);
-	}
-	if (s1.compare("") == 0)
+	o_file = dot_replace_open(path + ".replace");
+	if (o_file.is_open() == false)
 	{
-		std::cerr << "ERROR : 1st argument must not be empty !" << std::endl;
+		i_file.close();
 		return (1);
 	}
 	while (std::getline(i_file, buf))
 	{
-		while ((index = buf.find(s1, 0)) != -1)
+		while (!replacement_aborted && (index = buf.find(s1, 0)) != -1)
 		{
 			buf.erase(index, s1.length());
 			buf.insert(index, s2);
