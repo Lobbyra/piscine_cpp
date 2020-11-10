d11c5d603abf883588c96532fa7c39dac13a2078
lobbyra <jecaudal@student.42.fr>
2020-11-10 14:56:17 +0100

    :arrow_up: feat : cpp4 ex03 add makefile and code review
    Add comments in main

--- cpp4/ex03/main.cpp
+++ cpp4/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jecaudal <jecaudal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 15:34:30 by jecaudal          #+#    #+#             */
-/*   Updated: 2020/10/20 15:14:56 by jecaudal         ###   ########.fr       */
+/*   Updated: 2020/11/10 14:55:13 by jecaudal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,30 @@
 
 int		main(void)
 {
-	IMateriaSource	*src = new MateriaSource();
+	std::cout << "--------------" << std::endl;
+	std::cout << "Tests du sujet" << std::endl;
+	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
+	ICharacter* me = new Character("me");
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	ICharacter* bob = new Character("bob");
+	me->use(0, *bob);
+	me->use(1, *bob);
+	delete bob;
+	delete me;
+	delete src;
+	
+	std::cout << "------------------" << std::endl;
+	std::cout << "Tests additionnels" << std::endl;
+	// --------------------------------------------------------------------------
+	IMateriaSource	*source = new MateriaSource();
+	source->learnMateria(new Ice());
+	source->learnMateria(new Cure());
 
 	ICharacter	*moi = new Character("moi");
 
@@ -30,55 +51,64 @@ int		main(void)
 	AMateria	*tmp4;
 	AMateria	*tmp5;
 	AMateria	*tmp6;
-	tmp1 = src->createMateria("cure");
+	tmp1 = source->createMateria("cure");
 	moi->equip(tmp1);
-	tmp2 = src->createMateria("ice");
+	tmp2 = source->createMateria("ice");
 	moi->equip(tmp2);
-	tmp3 = src->createMateria("cure");
+	tmp3 = source->createMateria("cure");
 	moi->equip(tmp3);
-	tmp4 = src->createMateria("AAAAAAA");
+	tmp4 = source->createMateria("AAAAAAA"); // Test d'erreur sur un type de materia non existant
 	moi->equip(tmp4);
-	tmp5 = src->createMateria("cure");
+	tmp5 = source->createMateria("cure");
 	moi->equip(tmp5);
-	tmp6 = src->createMateria("cure");
+	tmp6 = source->createMateria("cure"); // Test d'erreur sur un materia en trop sur 4
 	moi->equip(tmp6);
 	std::cout << *((Character*)moi) << std::endl;
+	// --------------------------------------------------------------------------
 
-	ICharacter	*bob = new Character("bob");
+	// --------------------------------------------------------------------------
+	ICharacter	*jean = new Character("jean");
 
-	moi->use(0, *bob);
-	moi->use(1, *bob);
+	moi->use(0, *jean); // Test d'augmentation de l'xp sur un character vide
+	moi->use(1, *jean); // Test d'augmentation de l'xp sur un character vide
 
-	std::cout << *((Character*)moi) << std::endl;
-	ICharacter	*jerem = new Character(*(Character*)moi);
+	std::cout << *((Character*)moi) << std::endl; // Affichage de moi pour voir l'augmentation de l'xp
+	ICharacter	*jerem = new Character(*(Character*)moi); // Test de constructeur par copie
+	// --------------------------------------------------------------------------
 
-	((Character*)jerem)->setName("jerem");
-	std::cout << *((Character*)jerem) << std::endl;
+	// --------------------------------------------------------------------------
+	((Character*)jerem)->setName("jerem"); // Test du changement de nom
+	std::cout << *((Character*)jerem) << std::endl; // Affichage de jerem pour voir si le nom a changé
+	// --------------------------------------------------------------------------
 
-	moi->unequip(0);
+	// --------------------------------------------------------------------------
+	moi->unequip(0); // Unequip pour voir si la materia se supprime dans jerem
 	moi->unequip(1);
 	moi->unequip(2);
 	moi->unequip(3);
 
-	jerem->use(2, *bob);
-	jerem->use(3, *bob);
-	jerem->use(3, *bob);
-	jerem->use(3, *bob);
-	jerem->use(3, *bob);
-	jerem->use(3, *bob);
+	jerem->use(2, *jean); // Test des materia de jerem pour valider leurs existence
+	jerem->use(3, *jean);
+	jerem->use(3, *jean);
+	jerem->use(3, *jean);
+	jerem->use(3, *jean);
+	jerem->use(3, *jean);
 	std::cout << *((Character*)moi) << std::endl;
 	std::cout << *((Character*)jerem) << std::endl;
+	// --------------------------------------------------------------------------
 
-	ICharacter	*copy_jerem = new Character();
+	// --------------------------------------------------------------------------
+	ICharacter	*copy_jerem = new Character(); // Test de l'assignation par copie
 
 	*(Character*)copy_jerem = *(Character*)jerem;
 	((Character*)copy_jerem)->setName("copy_jerem");
 
 	std::cout << *((Character*)copy_jerem) << std::endl;
+	// --------------------------------------------------------------------------
 
-	delete bob;
+	delete jean;
 	delete moi;
-	delete src;
+	delete source;
 	delete jerem;
 	delete copy_jerem;
 	delete tmp1;
