fd5c1c3dc96b21e9568a454854f3e7955cba4b2c
lobbyra <jecaudal@student.42.fr>
2020-11-17 18:02:21 +0100

    :arrow_up: cpp6 ex00 add overflow limit

--- cpp6/ex00/main.cpp
+++ cpp6/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jecaudal <jecaudal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 11:58:13 by jecaudal          #+#    #+#             */
-/*   Updated: 2020/10/26 15:42:06 by jecaudal         ###   ########.fr       */
+/*   Updated: 2020/11/17 17:41:38 by jecaudal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,12 @@ static void	print_usage(void)
 static bool	str_is_convertible(std::string str)
 {
 	bool					dot_saw = false;
-	bool					is_positive = true;
 	std::string::iterator	it = str.begin();
 
 	if (str.length() == 1 && str[0] >= ASCII_MIN_PRINTABLE && str[0] <= ASCII_MAX_PRINTABLE)
 		return (true);
 	while (it != str.end() && (it[0] == '+' || it[0] == '-'))
-	{
-		if (it[0] == '+')
-			is_positive = true;
-		else if (it[0] == '-')
-			is_positive = false;
 		it++;
-	}
 	if (isdigit(it[0]) == false)
 		return (false);
 	while (it != str.end())
